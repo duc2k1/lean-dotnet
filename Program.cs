6bf40407c859fc05a463f51b1844aa0c1b467c39
duc2k1 <duc.phan622@gmail.com>
2022-03-31 10:45:08 +0700

    Init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My friend");
+        }
+    }
+}
