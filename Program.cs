b4223a791d354595f8ffe128814dd4543afc3f5d
duc2k1 <duc.phan622@gmail.com>
2022-04-09 22:05:42 +0700

    update

--- Program.cs
+++ Program.cs
@@ -4,19 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Animal dog = new Dog();
-            dog.Speaking();
-        }
-        class Dog : Animal
-        {
-            public override void Speaking()
-            {
-                System.Console.WriteLine("Dog speaking");
-            }
-        }
-        abstract class Animal
-        {
-            public abstract void Speaking();
+            // all type variable 
+            // int, double, string, bool, char, byte, short, long, float, decimal
+            int a = 10;
+            double b = 10.5;
+            string c = "Hello";
+            bool d = true;
+            char e = 'a';
+            byte f = 10;
+            short g = 10;
+            long h = 10;
+            float i = 10.5f;
+            decimal j = 10.5m;
+            // writeLine all type variable
+            Console.WriteLine($"{a} {b} {c} {d} {e} {f} {g} {h} {i} {j}");
         }
     }
 }
