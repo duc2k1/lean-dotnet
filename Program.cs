35fa9ac7b3036379ec4e46be8a0ed1048d615f30
duc2k1 <duc.phan622@gmail.com>
2022-04-10 08:40:45 +0700

    update

--- Program.cs
+++ Program.cs
@@ -4,20 +4,14 @@
     {
         static void Main(string[] args)
         {
-            // all type variable 
-            // int, double, string, bool, char, byte, short, long, float, decimal
-            int a = 10;
-            double b = 10.5;
-            string c = "Hello";
-            bool d = true;
-            char e = 'a';
-            byte f = 10;
-            short g = 10;
-            long h = 10;
-            float i = 10.5f;
-            decimal j = 10.5m;
-            // writeLine all type variable
-            Console.WriteLine($"{a} {b} {c} {d} {e} {f} {g} {h} {i} {j}");
+            int[] a = new int[10] { 3, 2, 5, 1, 4, 6, 7, 8, 9, 10 };
+            a = a.OrderBy(x => x).ToArray();
+            Console.WriteLine(string.Join(" ", a));
+            double[,] b = new double[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        }
+        static void HelloWorld()
+        {
+            System.Console.WriteLine("hello world");
         }
     }
 }
